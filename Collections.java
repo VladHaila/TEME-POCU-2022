95de5c8c2763836e0519168e0779c25bd39af215
Vlad Hailă <99145328+VladHaila@users.noreply.github.com>
2022-07-18 23:48:12 +0300

    Homework for Collections ( unfinished )

--- Collections.java
+++ Collections.java
@@ -1,7 +1,13 @@
+import java.sql.Array;
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Collections {
     public static void Ex1(){
+
+        //Generate 10 random numbers between 1 and 20,
+        // which require that the random numbers cannot be repeated
 
         int count=0;
         int[] randomnumbers = new int[10];
@@ -27,8 +33,44 @@ public class Collections {
         }
         System.out.println(Arrays.toString(randomnumbers));
     }
+    
+    public static void Ex2()
+    {
+        // Use Scanner to read a line of input from the keyboard, remove duplicate characters,
+        // and print out different characters // print result + duplicates
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insert a line: ");
+        String word = scanner.nextLine().trim();
+        System.out.println("Inserted word: " + word);
+        char[] wordedit = new char[word.length()];
+        int i,j, a=word.length();
+        for (i=0;i<a;i++)
+        {
+            wordedit[i] = word.charAt(i); // inserted the content of the read line into the array
+        }
+        //System.out.println("The word is: "+ Arrays.toString(wordedit)); // Checking if the array is correct
+        boolean OK = true;
+        System.out.print("The Word without duplicates: ");
+        for(i=0;i<a;i++)
+        {
+            OK= true;
+                for (j = 1; j <= i; j++)
+                {
+                    if(wordedit[i]==wordedit[j-1])
+                    {
+                        OK = false;
+                    }
+                }
+            if(OK)
+            {
+                System.out.print(wordedit[i]);
+            }
+        }
+    }
     public static void main(String[] args)
     {
         Ex1();
+        Ex2();
     }
 }
